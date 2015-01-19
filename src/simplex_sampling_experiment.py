baa4c8360573ad413775d7ef12e75e4fe37b3594
Patrick O'Neill <pon2@umbc.edu>
2015-01-18 23:45:43 -0500

    flow diagrams

--- src/simplex_sampling_experiment.py
+++ src/simplex_sampling_experiment.py
@@ -4,15 +4,16 @@ What is the most efficient way to pack info into a site: correlation or conserva
 Fundamental conservation: total_mi(ps) + h_np(ps) + ic(ps) = 2*w
 """
 from mpl_toolkits.mplot3d import Axes3D
-from utils import simplex_sample,h,norm,dot,transpose,log2,interpolate,pl,fac
+from utils import simplex_sample,h,norm,dot,transpose,log2,interpolate,pl,fac,maybesave,choose2
 from itertools import product,permutations
 from tqdm import tqdm
 import numpy as np
-from math import log,exp,sqrt,acos,pi,cos,sin
+from math import log,exp,sqrt,acos,pi,cos,sin,gamma
 from matplotlib import pyplot as plt
 from scipy.stats import pearsonr,spearmanr
 import sys
 import random
+from collections import defaultdict
 sys.path.append("/home/pat/the_royal_we/src")
 from we import weighted_ensemble
 
@@ -21,9 +22,26 @@ base_index = {b:k for k,b in enumerate("ACGT")}
 def sample(num_cols):
     return np.array(simplex_sample(4**num_cols))
 
-def h_np(ps):
+def h_np_ref(ps):
     """Return entropy in bits"""
     return -np.sum(ps*np.log(ps))/log(2)
+
+def h_np(ps):
+    """Return entropy in bits"""
+    psp = ps[ps>0]
+    return -np.sum(psp*np.log(psp))/log(2)
+
+def dkl(ps,qs):
+    return np.sum(ps*np.log(ps/qs))
+
+def queify(ps,M=None):
+    psfm = marginalize(ps)
+    qs = np.zeros(len(ps))
+    for k,idxs in enumerate(product(*[range(4) for i in range(len(psfm))])):
+        log_q = sum(log(psfm[j][idx]) for j,idx in enumerate(idxs))
+        qs[k] = exp(log_q)
+    return qs
+        
     
 def marginalize_ref(ps):
     """turn ps into a psfm"""
@@ -46,7 +64,7 @@ def marginalize(ps,M=None):
 
 def make_kmers(w):
     return (product(*["ACGT" for i in range(w)]))
-    
+
 def marginalization_matrix_ref(w):
     kmers = make_kmers(w)
     M = np.zeros((4*w,4**w))
@@ -77,6 +95,9 @@ def ic(ps,M=None):
     psfm = marginalize(ps,M)
     return sum(2-h(col) for col in psfm)
 
+def total_ic(ps):
+    return log2(len(ps)) - h_np(ps)
+    
 def psfm_entropy(ps,M=None):
     psfm = marginalize(ps,M)
     return sum(h(col) for col in psfm)
@@ -88,13 +109,66 @@ def total_mi(ps,M=None):
     psfm = marginalize(ps,M)
     w = len(psfm)
     dkl = 0
-    for k,kmer in tqdm(enumerate(make_kmers(w)),total=len(ps)):
+    for k,kmer in enumerate(make_kmers(w)):
         p = ps[k]
         logq = sum([log(psfm[i][base_index[c]]) for i,c in enumerate(kmer)])
         dkl += p * (log(p)-logq)
     return dkl/log(2)
 
-def plot_h_vs_ic(L,trials,sigma=1,max_h=None,M=None):
+def pairwise_mi_ref(ps,M=None):
+    w = int(log(len(ps),4))
+    dimer_freqs = defaultdict(lambda: np.zeros(16))
+    dimer_base_index = {"".join(comb):k for k,comb in enumerate(product("ACGT","ACGT"))}
+    psfm = marginalize(ps,M)
+    for k,(kmer,p) in enumerate(zip(make_kmers(w),ps)):
+        for (i,j) in choose2(range(w)):
+            dimer = kmer[i] + kmer[j]
+            dimer_freqs[(i,j)][dimer_base_index[dimer]]+=p
+    return sum(dimer_freqs[(i,j)][k]*log2(dimer_freqs[(i,j)][k]/(psfm[i][k/4]*psfm[j][k%4]))
+                      for k in range(16) for (i,j) in choose2(range(w)))
+
+
+def plot_ic_vs_pairwise_mi(L,sigmas=interpolate(0.01,10,100),max_h=None,M=None):
+    if max_h is None:
+        print "generating samples"
+        pss = [simplexify_sample(4**L,sigma=sigma)
+               for sigma in sigmas]
+    else:
+        pss = []
+        while len(pss) < trials:
+            ps = sample(L)
+            if h(ps) < max_h:
+                pss.append(ps)
+                print len(pss)
+    print "computing M"
+    if M is None:
+        M = marginalization_matrix(L)
+    print "computing ic"
+    total_ics = map(lambda ps:total_ic(ps),tqdm(pss))
+    psfm_ics = map(lambda ps:2*L-psfm_entropy(ps,M),tqdm(pss))
+    print "computing pairwise mi"
+    pair_mis = map(lambda ps:pairwise_mi_ref(ps,M),tqdm(pss))
+    print "computing total mi"
+    total_mis = map(lambda ps:total_mi(ps,M),tqdm(pss))
+    # print "computing columnwise entropies"
+    # plt.scatter(hs,hqs)
+    plt.scatter(psfm_ics,pair_mis,color='g',label='pair MI')
+    plt.scatter(psfm_ics,total_mis,label='total MI')
+    plt.scatter(psfm_ics,total_ics,color='r',label='Total IC')
+    #plt.plot([0,2*L],[2*L,0])
+    #plt.plot([0,2*L],[0,2*L])
+    # plt.plot([0,2],[0,4])
+    # plt.plot([0,2],[0,2*L])
+    # print pearsonr(ics,hs)
+    # print spearmanr(ics,hs)
+    # plt.plot([0,2*L],[0,2*L])
+    # plt.plot(*pl(lambda icp:L*icp+2*(L-L**2),[2*(L-1),2*L]))
+    plt.xlabel("PSFM IC")
+    plt.ylabel("Bits")
+    plt.title("Length=%s" % L)
+    plt.legend()
+    
+def plot_h_vs_ic(L,trials,sigma=1,max_h=None,M=None,xfunc=lambda ps:2*L):
     if max_h is None:
         print "generating samples"
         pss = [simplexify_sample(4**L,sigma=sigma)
@@ -145,6 +219,27 @@ def simplexify(p):
 
 def simplexify_sample(k,sigma=1):
     return simplexify(np.random.normal(0,sigma,k))
+
+def dirichlet_sample(k,alpha):
+    xs = np.random.gamma(alpha,1,k)
+    Z = np.sum(xs)
+    return xs/Z
+
+def dirichlet_sample2(k,alpha):
+    xs = np.array([random.gammavariate(alpha,1) for i in xrange(k)])
+    Z = np.sum(xs)
+    return xs/Z
+
+def validate_dirichlet_sample(Ks = [2,5,10,20,50,100],N=1000):
+    alphas = [10**i for i in interpolate(-5,0,100)]
+    for K in Ks:
+        print K
+        plt.scatter(*pl(lambda a:mean(h_np(dirichlet_sample(K,a)) for i in xrange(N)),alphas))
+        plt.plot(*pl(lambda alpha:expected_entropy(K,alpha=alpha),alphas),label="%s pred" % K)
+    plt.xlabel("alpha")
+    plt.ylabel("Entropy (bits)")
+    plt.semilogx()
+    plt.legend()
     
 def grad_ref(p):
     """Return gradient of entropy constrained to simplex"""
@@ -413,20 +508,33 @@ def plot_bin_col_ref(w,beta,N):
     plt.scatter(hps,hqs)
     plt.plot([0,2*w],[0,2*w])
 
-def plot_bin_col(w,beta,N):
+def plot_bin_col(w,beta,N,sort=True,color='b'):
     hps = []
     hqs = []
     K = int(4**w)
-    for i in tqdm(range(N)):
-        ps = simplexify_sample(K,beta)
-        ps.sort()
+    if not type(beta) is list:
+        beta = [beta]*N
+    for i in tqdm(xrange(N),total=N):
+        ps = simplexify_sample(K,beta[i])
+        if sort:
+            ps.sort()
         hps.append(h_np(ps))
         hqs.append(h_np(bin_col(ps)))
-    plt.scatter(hps,hqs)
-    plt.plot([0,2*w],[0,2*w])
+    #plt.scatter(hps,hqs,color=color,label="un"*(not sort) + "sorted")
+    plt.plot([0,2],[0,2],color='b')
+    plt.plot([2,2*w],[2,2],color='b')
+    plt.plot([2*w-2,2*w],[0,2],color='b')
+    plt.plot([0,2*w-2],[0,0])
+    plt.xlabel("H_P")
+    plt.ylabel("H_Q")
+    plt.title("Full vs. Marginal Entropy (K=4^%s)" % w)
+    plt.legend()
     return hps,hqs    
 
-        
+def make_bin_col_plot(filename=None):
+    plot_bin_col(10,interpolate(.01,10,100),100,sort=True,color='b')
+    plot_bin_col(10,interpolate(.01,10,100),100,sort=False,color='g')
+    maybesave(filename)
 def entropy_spectrum(ps,N):
     """
     Sample psfm entropy spectrum, the set of entropies of psfms
@@ -464,5 +572,103 @@ def knuth_shuffle(K):
         j = random.randrange(K)
         xs[i],xs[j] = xs[j],xs[i]
     return xs
+
+def expected_entropy_ref(K,num_points):
+    """Compute expected entropy of uniform prior on probability simplex on
+    K elements through numerical integration"""
+    def f(x):
+        return -x*log(x) if x > 0 else 0
+    def integrand(x):
+        return f(x)*(1-x)**(K-2)
+    return K*(K-1)*mean(integrand(x) for x in interpolate(0,1,num_points))/log(2)
+
+def expected_entropy(K,alpha=1,num_points=10000):
+    """Compute expected entropy of uniform prior on probability simplex on
+    K elements through numerical integration"""
+    def phi(x):
+        return -x*log(x) if x > 0 else 0
+    def integrand(x):
+        if x == 0 or x == 1:
+            return 0
+        else:
+            return phi(x)*x**(alpha-1)*(1-x)**((K-1)*alpha-1)
+    prefactor = K*gamma(K*alpha)/(gamma(alpha)*gamma((K-1)*alpha))
+    #prefactor = K/(gamma(alpha))*(alpha*(K-1))**alpha
+    #print prefactor,approx_prefactor
+    integral = mean(integrand(x) for x in interpolate(0,1,num_points))/log(2)
+    return prefactor*integral
+
+def mutation_matrix_ref(mu,w,mode="continuous"):
+    "mutation matrix for w columns at rate mu"
+    K = int(4**w)
+    M = np.zeros((K,K))
+    if mode == "continuous":
+        """M is a transition rate matrix"""
+        res = 0
+    else:
+        """M is a stochastic matrix"""
+        res = 1
+    for i,kmer_i in enumerate(make_kmers(w)):
+        for j,kmer_j in enumerate(make_kmers(w)):
+            distance = w - sum(zipWith(lambda x,y:x==y,kmer_i,kmer_j))
+            if distance == 0:
+                M[i][j] = res*1 - 3*w*mu
+            elif distance == 1:
+                M[i][j] = mu
+    return M
+
+def mutate_ref(ps,t,mu=None,M=None):
+    if M is None:
+        L = int(log(len(ps),4))
+        M = mutation_matrix_ref(mu,L)
+    return scipy.linalg.expm(M*t).dot(ps)
+
+def mutate(ps,t,M):
+    eigvals,eigvecs = np.linalg.eig(M)
+    Q = np.matrix(eigvecs)
+    return Q.dot(np.diag(np.exp(eigvals*t))).dot(np.linalg.inv(Q)).dot(ps)
+
+def flow_diagram(L,sigmas=interpolate(0.0005,0.01,10),steps=100):
+    pss = [dirichlet_sample(4**L,sigma) for sigma in sigmas]
+    Mut = mutation_matrix_ref(0.01,L,mode='discrete')
+    Mar = marginalization_matrix(L)
+    delta_icps = []
+    delta_icqs = []
+    for sigma in sigmas:
+        ps = dirichlet_sample(4**L,sigma)
+        pps = queify(ps)
+        icps = [total_ic(ps)]
+        icqs = [2*L-psfm_entropy(ps,Mar)]
+        icpps = [total_ic(pps)]
+        icqps = [2*L-psfm_entropy(pps,Mar)]
+        for step in xrange(steps):
+            ps = Mut.dot(ps)
+            pps = Mut.dot(pps)
+            icps.append(total_ic(ps))
+            icqs.append(2*L-psfm_entropy(ps,Mar))
+            icpps.append(total_ic(pps))
+            icqps.append(2*L-psfm_entropy(pps,Mar))
+        plt.plot(icps,icqs,color='r')
+        plt.plot(icpps,icqps,color='g')
+    plt.plot([0,2*L-2],[0,0],color='b')
+    plt.plot([0,2],[0,2*L],color='b')
+    plt.plot([2,2*L],[2*L,2*L],color='b')
+    plt.plot([2*L-2,2*L],[0,2*L],color='b')
+    plt.xlabel("ICp")
+    plt.ylabel("ICq")
+    plt.show()
+        
+def mutation_matrix_experiment(L,N):
+    mu = 0.01
+    time = 1
+    print "sampling"
+    pss = [dirichlet_sample(4**L,0.1) for i in xrange(N)]
+    Mut = mutation_matrix_ref(mu,L)
+    M = marginalization_matrix(L)
+    print "dkl_mut"
+    dkl_muts = [dkl(ps,mutate(ps,time,M=Mut)) for ps in tqdm(pss)]
+    print "dkl_qs"
+    dkl_qs = [dkl(ps,queify(ps,M)) for ps in pss]
+    plt.scatter(dkl_qs,dkl_muts)
     
 print "loaded"
